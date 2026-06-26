63c6917905ee30b09a6d36f1b01687f81f47255a
Ralston E. Lee Smith <106720518+ralstonleesmith@users.noreply.github.com>
2026-06-26 11:27:23 -0400

    fix: default managed startup file to production mode

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require('next');
 
 const port = Number(process.env.PORT || 3000);
 const hostname = process.env.HOST || process.env.HOSTNAME || '0.0.0.0';
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV === 'development';
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
